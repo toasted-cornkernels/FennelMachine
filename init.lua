e458b2cc164f77035f2a3f0fb9aacb1d2c5c74ee
이정수 <jslee@ijeongsus-iMac.attlocal.net>
2022-12-21 14:07:06 -0800

    initial

---
+++ init.lua
@@ -0,0 +1,23 @@
+hs.alert.show("Spacehammer config loaded")
+
+-- Support upcoming 5.4 release and also use luarocks' local path
+package.path = package.path .. ";" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.4/?.lua;" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.4/?/init.lua"
+package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.4/?.so"
+package.path = package.path .. ";" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.3/?.lua;" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.3/?/init.lua"
+package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.3/?.so"
+
+fennel = require("fennel")
+table.insert(package.loaders or package.searchers, fennel.searcher)
+
+require "core"
+
+local VimMode = hs.loadSpoon('VimMode')
+local vim = VimMode:new()
+
+vim
+   :disableForApp('Code')
+   :disableForApp('MacVim')
+   :disableForApp('Emacs')
+   :disableForApp('Terminal')
+   :disableForApp('zoom.us')
+   :bindHotKeys({ enter = {{'option'}, '[' }})
