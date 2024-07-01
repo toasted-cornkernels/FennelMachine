0d496cf878815d0353e13f72206ec75d5ff19ba7
toasted-cornkernels <volcano0909@gmail.com>
2024-06-30 19:20:42 -0700

    update

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ package.path = package.path .. ";" .. os.getenv("HOME") .. "/.luarocks/share/lua
 package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.4/?.so"
 package.path = package.path .. ";" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.3/?.lua;" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.3/?/init.lua"
 package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.3/?.so"
+package.path = package.path .. ";" .. os.getenv("HOME") .. "/.nix-profile/share/lua/5.3/?.lua;" .. os.getenv("HOME") .. "/.nix-profile/share/lua/5.3/?/init.lua"
 
 fennel = require("fennel")
 table.insert(package.loaders or package.searchers, fennel.searcher)
